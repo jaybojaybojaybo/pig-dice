dfeb2ea7b7b699d49eaf1ff5f6069de6dcd6fc3b
Jasun Feddema and Marilyn Carlin <jasun@>
2017-03-27 18:50:38 -0700

    add TurnPlayer object to determine player turn between two user players.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // business logic
 function Player(name, turnScore, playerScore) {
-  this.name = name;
+  this.playerName = name;
   this.turnScore = turnScore;
   this.playerScore = playerScore;
 }
@@ -9,9 +9,17 @@ function Die(sides) {
   sides = [1, 2, 3, 4, 5, 6]
 }
 
+function TurnPlayer(player1, player2, turnCount) {
+  if (turnCount % 2 === 0) {
+    player2Turn = true;
+  } else {
+    player1Turn = true;
+  }
+}
 
 // front end logic
 
 var name;
-var turnScore;
-var playerScore;
+var turnScore = 0;
+var playerScore = 0;
+var turnCount = 1;
