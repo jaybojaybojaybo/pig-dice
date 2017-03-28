fa1c5075d61b18268f8b0ec6703d465dec804f4b
Jasun Feddema and Marilyn Carlin <marilyn@>
2017-03-27 20:31:33 -0700

    add Die prototype to generate random number from Die array and add number to player turnScore, abiding by the rules of Pig Dice.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 // business logic
+var turnScore = 0;
+var playerScore = 0;
+var turnCount = 1;
+
 function Player(name) {
   this.playerName = name;
   this.turnScore = 0;
@@ -9,6 +13,8 @@ function Die(sides) {
   this.sides = [1, 2, 3, 4, 5, 6]
 }
 
+var die = new Die();
+
 function TurnPlayer(player1, player2, turnCount) {
   if (turnCount % 2 === 0) {
     player2Turn = true;
@@ -17,22 +23,38 @@ function TurnPlayer(player1, player2, turnCount) {
   }
 }
 
+Die.prototype.rollDie = function() {
+  var rolledSide = this.sides[Math.floor(Math.random() * this.sides.length)];
+  if (rolledSide === 1) {
+    turnScore = 0
+    turnCount += 1;
+  } else {
+    turnScore += rolledSide;
+  }
+}
+
 // front end logic
 $(document).ready(function() {
+  var player1;
+  var player2;
+
   $("form").submit(function(event) {
     event.preventDefault();
 
-  var turnScore = 0;
-  var playerScore = 0;
-  var turnCount = 1;
-
   var player1Name = $("input#player1Name").val();
-  var player1 = new Player(player1Name, turnScore, playerScore);
+  player1 = new Player(player1Name, turnScore, playerScore);
 
   var player2Name = $("input#player2Name").val();
-  var player2 = new Player(player2Name, turnScore, playerScore);
+  player2 = new Player(player2Name, turnScore, playerScore);
 
-  // console.log(player1);
-  // console.log(player2);
+  console.log(player1);
+  console.log(player2);
   });
+
+  $("#rollButton").click(function(){
+    die.rollDie();
+    console.log(turnScore);
+    console.log(player1);
+    console.log(player2);
+  })
 });
