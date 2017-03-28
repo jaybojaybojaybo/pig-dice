f03bc448f5716ac7b9611e9cfd49bf555fb8357c
Jasun Feddema and Marilyn Carlin <jasun@>
2017-03-27 18:43:26 -0700

    add initial html document to create pig dice website, as well as initial js text to create Player and Die objects in the business logic.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// business logic
+function Player(name, turnScore, playerScore) {
+  this.name = name;
+  this.turnScore = turnScore;
+  this.playerScore = playerScore;
+}
+
+function Die(sides) {
+  sides = [1, 2, 3, 4, 5, 6]
+}
+
+
+// front end logic
+
+var name;
+var turnScore;
+var playerScore;
